e1c9aeea7e629d4cf3a32b3afebef3b069c1ba5c
Ashish Mishra <ashish_mishra@outlook.com>
2017-11-05 01:07:37 -0400

    Refactored code

--- websocketclient/src/main/java/socket/StndAloneClient.java
+++ websocketclient/src/main/java/socket/StndAloneClient.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package upnp.socket.client;
+package socket;
 
 import java.net.URI;
 import javax.websocket.ClientEndpoint;
