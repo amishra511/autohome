39db3a357898b9024d8d1261b102c583973906a8
Ashish Mishra <ashish_mishra@outlook.com>
2017-11-09 22:38:40 -0500

    Formatted index.html

--- server/websocket/src/main/webapp/websocketclientscript.js
+++ server/websocket/src/main/webapp/websocketclientscript.js
@@ -5,17 +5,13 @@
  */
 
 //var wsUri = "ws://" + document.location.host + document.location.pathname + "endpoint";
-var wsUri = "ws://" + document.location.host + document.location.pathname + "upnpendpoint";
+var wsUri = "ws://" + document.location.host + document.location.pathname + "upnpendpoint";//If running with server app
 var websocket = new WebSocket(wsUri);
 
 //Function that outputs a message on webpage
 function writeToScreen(message) {
     output.innerHTML += message + "<br>";
 }
-
-//Adds an event listener to canvas element when it is clicked
-var canvas = document.getElementById("myCanvas");
-canvas.addEventListener("click", getText, false);
 
 //Payload json
 //{"request":{"operation":"test","deviceId":"dev","deviceState":"state"},"response":null}
@@ -27,27 +23,11 @@ canvas.addEventListener("click", getText, false);
         "deviceState": "1"},
     "response":null
     });
-function getText(evt){
-    var d = new Date();
-    var n = d.getTime();
-    console.log("Inside get text");
-    sendText(reqDiscover);
-}
-function sendText(json) {
-    console.log("sending text: " + json);
-    websocket.send(json);
-}
 
 
 //Adds an event listener to button when it is clicked
 var btnDisc = document.getElementById("btnDisc");
 btnDisc.addEventListener("click", sendDiscoveryReq, false);
-
-//  var discvryReq = JSON.stringify({
-//        "operation": "discover",
-//        "deviceId": "",
-//        "deviceState": ""
-//    });
 
 function sendDiscoveryReq(){
     console.log("--sending discovery request--");
@@ -64,8 +44,6 @@ function onError(evt) {
 var output = document.getElementById("output");
 websocket.onopen = function(evt) { onOpen(evt) };
 
-
-
 function onOpen(evt) {
     writeToScreen("Connected to " + wsUri);
 }
@@ -75,6 +53,4 @@ websocket.onmessage = function(evt) { onMessage(evt) };
 function onMessage(evt) {
     console.log("received: " + evt.data);
     writeToScreen("Message received: " + evt.data);
-//    drawImageText(evt.data);
 }
-// End test functions
